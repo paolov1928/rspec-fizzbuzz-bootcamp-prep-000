66e1a660de5a12b5337c7f639d95aeb819faf13c
ventura.paolo <ventura.paolo@gmail.com>
2018-09-17 22:29:23 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(n)
- if n % 3 == 0 
-   return "Fizz"
+ if n % 3 == 0 (n % 3 == 0 && n % 5 == 0)
+   return "FizzBuzz"
  elsif n % 5 == 0
  return "Buzz"
- elsif (n % 3 == 0 && n % 5 == 0)
- return "FizzBuzz"
+ elsif n % 3 == 0
+ return "Fizz"
  end
 end
